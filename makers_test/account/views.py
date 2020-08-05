28abb716d9444f3fd7f9f55f01d3b32b6d73d129
addirossi <addirossi@gmail.com>
2020-08-05 11:57:26 +0600

    Add auth

--- makers_test/account/views.py
+++ makers_test/account/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import RegistrationForm
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = RegistrationForm
+    template_name = 'account/registration.html'
